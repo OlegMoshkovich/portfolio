7d17b738251c82df228f715b2d03e0e39a9639c5
OlegMoshkovich <omoshkovich@gmail.com>
2015-06-26 13:32:23 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require("express");
+
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function(req,res){
+       res.sendFile(__dirname + '/public/hello.html');
+});
+
+app.get('/hello', function(req,res){
+      res.sendFile(__dirname+'/public/hello.html');
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+  console.log(" I am listening on some port");
+});

8fb24b9ef86d240e352d3b959e265b5626944c81
OlegMoshkovich <omoshkovich@gmail.com>
2015-10-05 01:53:06 +0000

    worked out dependency diagram, inplimented simple graph

--- server.js
+++ server.js
@@ -24,6 +24,23 @@ app.get('/hc-dependencies', function(req,res){
        res.sendfile(__dirname + '/public/hc-dependencies.html');
 });
 
+app.get('/hc-analytics', function(req,res){
+       res.sendfile(__dirname + '/public/hc-analytics.html');
+});
+
+app.get('/edgeBinding', function(req,res){
+       res.sendfile(__dirname + '/public/edgeBinding.html');
+});
+
+app.get('/tutorial', function(req,res){
+       res.sendfile(__dirname + '/public/d3-tutorial.html');
+});
+
+app.get('/simplechart', function(req,res){
+       res.sendfile(__dirname + '/public/simple_chart.html');
+});
+
+
 
 app.listen(process.env.PORT, process.env.IP, function(){
   console.log(" I am listening on some port");

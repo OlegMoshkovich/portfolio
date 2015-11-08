43dc12269b483ef2dd511f73fd2b13e0ee3cde06
OlegMoshkovich <omoshkovich@gmail.com>
2015-11-08 02:45:01 +0000

    added important map

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
+var fs = require('fs');
+var readline = require('readline');
+var google = require('googleapis');
+var googleAuth = require('google-auth-library');
+
+var SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly'];
+var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
+    process.env.USERPROFILE) + '/.credentials/';
+var TOKEN_PATH = TOKEN_DIR + 'drive-nodejs-quickstart.json';
+
+
+
+
+
+
 var express = require("express");
 
 var app = express();
@@ -16,13 +31,13 @@ app.get('/hello', function(req,res){
 //        res.sendfile(__dirname + '/public/hc.html');
 // });
 
-// app.get('/radial', function(req,res){
-//        res.sendfile(__dirname + '/public/radial.html');
-// });
+app.get('/radial', function(req,res){
+        res.sendfile(__dirname + '/public/sample_radial hierarchy.html');
+});
 
-// app.get('/hc-dependencies', function(req,res){
-//        res.sendfile(__dirname + '/public/hc-dependencies.html');
-// });
+app.get('/hc-dependencies', function(req,res){
+        res.sendfile(__dirname + '/public/hc-dependencies.html');
+});
 
 // app.get('/hc-analytics', function(req,res){
 //        res.sendfile(__dirname + '/public/hc-analytics.html');
@@ -40,19 +55,32 @@ app.get('/hello', function(req,res){
 //        res.sendfile(__dirname + '/public/simple_chart.html');
 // });
 
-// app.get('/hcs-ecosystem-current', function(req,res){
-//        res.sendfile(__dirname + '/public/hcs-ecosystem-current.html');
-// });
+app.get('/hcs-ecosystem-current', function(req,res){
+        res.sendfile(__dirname + '/public/hcs-ecosystem-current.html');
+});
 
-// app.get('/hcs-ecosystem-future', function(req,res){
-//        res.sendfile(__dirname + '/public/hcs-ecosystem-future.html');
-// });
+app.get('/hcs-ecosystem-future', function(req,res){
+        res.sendfile(__dirname + '/public/hcs-ecosystem-future.html');
+});
 
-// app.get('/hcs', function(req,res){
-//        res.sendfile(__dirname + '/public/hcs.html');
-// });
+app.get('/hcs', function(req,res){
+        res.sendfile(__dirname + '/public/hcs.html');
+});
+
+app.get('/important', function(req,res){
+        res.sendfile(__dirname + '/public/hc_important.html');
+});
+
+app.get('/google', function(req,res){
+        res.sendfile(__dirname + '/public/google.html');
+});
+
+
+
+
 
 
 app.listen(process.env.PORT, process.env.IP, function(){
   console.log(" I am listening on some port");
 });
+
